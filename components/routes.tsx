ed91a88399304fbdeb43b1dadc5c17a1826bd218
lizzie Metcalfe <lizzie@creativeco.io>
2024-10-14 19:02:04 +0200

    Replace Horizon AI with Wombat and remove PRO section from sidebar

--- components/routes.tsx
+++ components/routes.tsx
@@ -2,7 +2,7 @@
 import { IRoute } from '@/types/types';
 import {
   HiOutlineHome,
-  HiOutlineCpuChip,
+  HiOutlineChatBubbleLeftRight,
   HiOutlineUsers,
   HiOutlineUser,
   HiOutlineCog8Tooth,
@@ -22,7 +22,7 @@ export const routes: IRoute[] = [
     name: 'AI Chat',
     path: '/dashboard/ai-chat',
     icon: (
-      <HiOutlineCpuChip className="-mt-[7px] h-4 w-4 stroke-2 text-inherit" />
+      <HiOutlineChatBubbleLeftRight className="-mt-[7px] h-4 w-4 stroke-2 text-inherit" />
     ),
     collapse: false
   },
@@ -34,56 +34,4 @@ export const routes: IRoute[] = [
     ),
     collapse: false
   },
-  {
-    name: 'AI Generator',
-    path: '/dashboard/ai-generator',
-    icon: (
-      <HiOutlineDocumentText className="-mt-[7px] h-4 w-4 stroke-2 text-inherit" />
-    ),
-    collapse: false,
-    disabled: true
-  },
-  {
-    name: 'AI Assistant',
-    path: '/dashboard/ai-assistant',
-    icon: <HiOutlineUser className="-mt-[7px] h-4 w-4 stroke-2 text-inherit" />,
-    collapse: false,
-    disabled: true
-  },
-  {
-    name: 'Users List',
-    path: '/dashboard/users-list',
-    icon: (
-      <HiOutlineUsers className="-mt-[7px] h-4 w-4 stroke-2 text-inherit" />
-    ),
-    collapse: false,
-    disabled: true
-  },
-  {
-    name: 'Subscription',
-    path: '/dashboard/subscription',
-    icon: (
-      <HiOutlineCreditCard className="-mt-[7px] h-4 w-4 stroke-2 text-inherit" />
-    ),
-    collapse: false,
-    disabled: true
-  },
-  {
-    name: 'Landing Page',
-    path: '/home',
-    icon: (
-      <HiOutlineDocumentText className="-mt-[7px] h-4 w-4 stroke-2 text-inherit" />
-    ),
-    collapse: false,
-    disabled: true
-  },
-  {
-    name: 'Pricing Page',
-    path: '/pricing',
-    icon: (
-      <HiOutlineCurrencyDollar className="-mt-[7px] h-4 w-4 stroke-2 text-inherit" />
-    ),
-    collapse: false,
-    disabled: true
-  }
 ];
